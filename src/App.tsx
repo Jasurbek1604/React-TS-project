7ec29a0d69645ae4530a0a1671f36a68d2517958
Jasurbek <jasurdev1604@gmail.com>
2023-10-02 03:44:02 +0500

    Shopping Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Store from "./pages/Store";
+import styled from "styled-components";
+
+const Container = styled.div``;
 
 const App: React.FC = () => {
   return (
     <React.Fragment>
-      <h1>Jasurbek O'telbayev</h1>
+      <Container>
+        <Routes>
+          <Route element={<Navbar />}>
+            <Route path={"/"} element={<Home />} />
+            <Route path={"/store"} element={<Store />} />
+            <Route path={"/about"} element={<About />} />
+          </Route>
+        </Routes>
+      </Container>
     </React.Fragment>
   );
 };
