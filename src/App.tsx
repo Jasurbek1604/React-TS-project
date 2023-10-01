90af9ba1c42aff3dafd25ca2c41804c4254fc0c9
Jasurbek <jasurdev1604@gmail.com>
2023-10-02 02:41:48 +0500

    Shopping Card

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <h1>Jasurbek O'telbayev</h1>
+    </React.Fragment>
+  );
+};
+
+export default App;
