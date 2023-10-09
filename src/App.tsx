073f78d2f5ef05ab8b16e42be4e1bb14ea74ad1a
= <=>
2023-10-09 22:01:50 +0500

    TS

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,21 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Store from "./pages/Store";
 import styled from "styled-components";
-import { ShoppingCardProvider } from "./context/ShoppingCardContext";
 
 const Container = styled.div``;
 
 const App: React.FC = () => {
   return (
     <React.Fragment>
-      <ShoppingCardProvider>
-        <Container>
-          <Routes>
-            <Route element={<Navbar />}>
-              <Route path={"/"} element={<Home />} />
-              <Route path={"/store"} element={<Store />} />
-              <Route path={"/about"} element={<About />} />
-            </Route>
-          </Routes>
-        </Container>
-      </ShoppingCardProvider>
+      <Container>
+        <Routes>
+          <Route element={<Navbar />}>
+            <Route path={"/"} element={<Home />} />
+            <Route path={"/store"} element={<Store />} />
+            <Route path={"/about"} element={<About />} />
+          </Route>
+        </Routes>
+      </Container>
     </React.Fragment>
   );
 };
